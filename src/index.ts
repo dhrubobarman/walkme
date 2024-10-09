fbf8bc2925addf1ec3d5f6146890a40e4b6f9276
dhrubobarman <dhrubobarman847@gmail.com>
2024-10-09 14:07:49 +0530

    exported types

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,3 @@
-import { createButton } from '@/UI/components';
-import { Walkme } from '@/Walkme';
-import { testData } from '@/tempData';
 import './style.scss';
 export * from '@/Walkme';
-
-const button = createButton({ variant: 'primary', innerHTML: 'Start', className: 'absolute left-0 top-0' });
-document.body.appendChild(button);
-
-const walkme = new Walkme({ stepData: testData[0] });
-button.onclick = () => walkme.start();
+export * from '@/types';
